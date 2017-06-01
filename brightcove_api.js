bd572de7c8ee15e872f68e7fe173ecf5a74a66b0
Andrew Lloyd <andrew.lloyd@tee2.org>
2017-06-01 11:11:40 -0700

    Refactor brightcove api to exportable module

--- brightcove_api.js
+++ brightcove_api.js
@@ -23,7 +23,7 @@ function requestNewAuthToken() {
 	        resolve(JSON.parse(body).access_token);
 	    } 
 		});	
-	});
+	}); 
 }
 
 function requestHelper(url, method, json) {
@@ -62,60 +62,43 @@ function requestHelper(url, method, json) {
 	return promise;
 }
 
-function searchVideosByTag(term, offset, callback) {
-	requestHelper(`${config.base_url_cms}/videos?q=tags:${term}&limit=100&offset=${offset}`, 'GET')
-		.then(body => {
-			console.log(body);
-		});
+function searchVideosByTag(term, offset) {
+	return requestHelper(`${config.base_url_cms}/videos?q=tags:${term}&limit=100&offset=${offset}`, 'GET');
 }
 
 function getIngestProfile(profile_id) {
 	profile_id = profile_id ? `/${profile_id}` : '';
-	requestHelper(`${config.base_url_ingestion}/profiles${profile_id}`, 'GET')
-		.then(body => {
-			console.log(body);
-		});
+	return requestHelper(`${config.base_url_ingestion}/profiles${profile_id}`, 'GET');
 }
 
 function retranscodeVideo(video_id) {
 	const body = {
     "master": { "use_archived_master": true },
-    "profile": "videocloud-mkc_watermark"
+    "profile": "videocloud-mkc_watermark",
+    "callbacks": ["http://[2600:100f:b017:dc70:78f2:356b:5173:6160]/ingest-notifications"]
 	}
-	requestHelper(`${config.base_url_dynamic_ingest}/${video_id}/ingest-requests`, 'POST', body)
-		.then(body => {
-			console.log(JSON.stringify(body, null, 2));
-		});
+	return requestHelper(`${config.base_url_dynamic_ingest}/${video_id}/ingest-requests`, 'POST', body);
 }
 
-/*
-http://docs.brightcove.com/en/video-cloud/di-api/getting-started/quick-start-di.html
-A 20 request per minute limit on each account for total requests across DI and the CMS API (CMS API or Ingest API requests)
-A limit of 100 concurrent DI jobs per account
-If you are ingesting files in batches, limit batches to 100 and wait for the previous batch to complete processing before continuing
-*/
-/////////////////////////////////////////// END FUNCTIONS ///////////////////////////////////////////
+var BrightcoveAPI = function (user, pass) {
+	
+};
 
+BrightcoveAPI.prototype = {
+    
+		searchVideosByTag: function(term, offset) {
+			return searchVideosByTag(term, offset);
+		},
 
+		getIngestProfile: function(profile_id) {
+			return getIngestProfile(profile_id);
+		},
 
-function getMkcVideosHelper() {
-	//const data = require('./mkc.json');
-	searchVideosByTag('mkc', 0, function(videos){
-			/*videos.forEach(function(item){
-				data.push(item);
-			});*/
-			console.log(videos);
-			//fs.writeFileSync('mkc.json', JSON.stringify(data, null, 2));
-			//console.log(require('./mkc.json').length);
-		});
+		retranscodeVideo: function(video_id) {
+			return retranscodeVideo(video_id);
+		}
+    
 }
 
-//getIngestProfile('59247af2e4b01c765233b9a7');//, require('./profile.json'));
-//retranscodeVideo('5227368064001');
-
-
-
-
-
-
+module.exports = BrightcoveAPI;
 
