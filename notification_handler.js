5cfa736981d9fcf1995a67b710313af051196a52
Andrew Lloyd <andrew.lloyd@tee2.org>
2017-06-02 17:17:02 -0700

    Add mock interface for notification handler

--- notification_handler.js
+++ notification_handler.js
@@ -3,6 +3,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const fs = require('fs')
+const LogInterface = require('./log_if')
+const log_if = new LogInterface()
+const UUID = require('./uuid');
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -10,9 +13,7 @@ app.listen(port);
 console.log('Server started! At http://localhost:' + port);
 
 // setup logging
-if (!fs.existsSync('log.json')) { 
-  fs.writeFileSync('log.json', '[]')
-}
+log_if.initialize('logs/notification_handler.json')
 
 const jobFinishedListeners = []
 
@@ -30,28 +31,21 @@ app.post('/api/ingest-notification', function(req, res) {
   	const errorMessage = req.body.errorMessage
 
   	// notify listeners of finished job
-  	jobFinishedListeners.forEach(listener => {
-  		listener(jobId)
-  	})
+  	notifyListeners(jobId)
 
   	// log the error message 
   	if (status === 'FAILED' || status !== 'SUCCESS') {
   		if (errorMessage) {
-  			logErrorMessage({date: new Date(), videoId: videoId, errorMessage: errorMessage})
+  			log_if.logErrorMessage({date: new Date(), videoId: videoId, jobId: jobId, errorMessage: errorMessage})
   		}
   	}
   }
 });
 
-function logErrorMessage(message) {
-	fs.readFile('log.json', (err, data) => {
-		if (err) {throw err}
-    var json = JSON.parse(data)
-    json.push(message)
-    fs.writeFile("log.json", JSON.stringify(json), err => {
-    	if (err) {throw err};
-    })
-	})
+function notifyListeners(jobId) {
+	jobFinishedListeners.forEach(listener => {
+  	listener(jobId)
+  })
 }
 
 var NotificationServer = function () {
@@ -66,15 +60,31 @@ NotificationServer.prototype = {
 
 		removeJobFinishedListener: function(listener) {
 			const index = jobFinishedListeners.indexOf(listener)
-			if (index > -1) {jobFinishedListeners.splice(index, 1)};
+			if (index > -1) {jobFinishedListeners.splice(index, 1)}
 		},
+
+		MOCK_retranscodeVideo: function(callback) {
+			MOCK_retranscodeVideo(callback)
+		}
     
 }
 
-module.exports = NotificationServer;
+module.exports = NotificationServer
+
+MOCK_jobsList = []
+
+function MOCK_retranscodeVideo(callback) {
+	setTimeout(callback => {
+		var uuid = UUID.generateUUID()
+		MOCK_jobsList.push(uuid)
+		callback({id: uuid})
+	}, 4000, callback)	
+}
 
 
-
+setInterval(()=>{
+	notifyListeners(MOCK_jobsList.pop())
+}, 5000)
 
 
 
